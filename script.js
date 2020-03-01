f5f5c31b51249f447283e75051bcb6c32ed7337f
Carol <carolkaji@gmail.com>
2020-03-01 09:23:43 -0300

    Inserido script.js e css para abrir e fechar regulamento

---
+++ script.js
@@ -0,0 +1,8 @@
+// linkRegulamento.style.visibility ='visible';
+linkRegulamento.addEventListener('click', function(){
+  regulamento.style.visibility = 'visible';
+});
+
+fechaContainer.addEventListener('click', function(){
+  regulamento.style.visibility = 'hidden';
+});
