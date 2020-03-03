1ff308b2247d4265c1721b51c267b5729475e062
Carol <carolkaji@gmail.com>
2020-03-03 20:45:09 -0300

    Adicionado botão limpar no cadastro.html. Alteração do Logo. Alteração no css. Adição da seção de agradecimento de cadastro

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-// linkRegulamento.style.visibility ='visible';
 linkRegulamento.addEventListener('click', function(){
   regulamento.style.visibility = 'visible';
 });
 
 fechaContainer.addEventListener('click', function(){
   regulamento.style.visibility = 'hidden';
+});
+
+cadastrar.addEventListener('click', function(){
+  confirmacao.style.visibility = 'visible';
+});
+
+limpar.addEventListener('click', function(){
+  let inputRadio = document.querySelectorAll('input[type="radio"]');
+  let checkbox = document.querySelector('input[type="checkbox"]');
+
+  nomeCompleto.value = '';
+  nascimento.value = '';
+  rgNumero.value = '';
+  rgDigito.value = '';
+  cpfNumero.value = '';
+  cpfDigito.value = '';
+  endereco.value = '';
+  enderecoComplemento.value = '';
+  email.value = '';
+  telefone.value = '';
+
+  for(var i = 0; i < inputRadio.length; i++){
+    inputRadio[i].checked = false;
+  }
+
+  checkbox.checked = false;
+
 });
